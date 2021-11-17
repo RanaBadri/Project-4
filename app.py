818fe93e7054f5b8de6a8ec184732404345d5610
Rana Badri <ranabadri01@gmail.com>
2021-11-16 20:14:49 -0700

    version 11.16.20

--- app.py
+++ app.py
@@ -3,46 +3,42 @@ import os
 import pandas as pd
 import json
 import requests
-
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+import pickle
 
 app = Flask(__name__)
 
 
 
-filepath = os.path.join("Resources", "Violent_and_Property_Crimes_Over_Time_data.csv")
-data = pd.read_csv(filepath)
-
+engine = create_engine(f'postgresql://postgres:postgres@localhost:5432/whitewine_db')
+data= pd.read_sql("select * FROM whitewine", con=engine) 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route("/bubble")
-def bubble():
-    return render_template('googleChart.html')
+# @app.route("/")
+# def bubble():
+#     return render_template()
 
 
-@app.route('/plot')
-def getData():
-    yeargroupedcrime = data.groupby("Year")[["Property crime rate", "Violent Crime rate"]].mean()
-    #yeargroupedcrime = yeargroupedcrime.reset_index()
-    return yeargroupedcrime.to_json()
+# @app.route('/plot')
+# def getData():
+    
+#     return yeargroupedcrime.to_json()
 
 
-@app.route('/plot2')
-def getData2():
-    regiongrouped = data.groupby("Region")["Violent Crime rate"].mean()
-    return regiongrouped.to_json()
+# @app.route('/plot2')
+# def getData2():
+#     return regiongrouped.to_json()
 
-@app.route('/plot3')
-@app.route('/plot3/<year>')
-def getData3(year=None):
-    if year:
-        yeardata = data.loc[data["Year"] == int(year)]
-    else:
-        yeardata = data
-    newDF = pd.DataFrame({"ID":yeardata["State"] + "-" + yeardata["Year"].astype(str), "Violent Crime":yeardata["Violent Crime rate"], "Property Crime": yeardata["Property crime rate"], "Region":yeardata["Region"], "Population":yeardata["Population"]})
-    return newDF.tail(50).to_json(orient='values')
+# @app.route('/plot3')
+# @app.route('/plot3/<year>')
+# def getData3(year=None):
+    
+#     return newDF.tail(50).to_json(orient='values')
 
 @app.route('/table')
 def maketable():
@@ -53,3 +49,5 @@ def maketable():
 
 if __name__ == '__main__':
     app.run(debug = True)
+
+    model = pickle.load("Resources/winequality.pkl")
